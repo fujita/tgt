a80adf606bc4f2e6d8112d20738f201e5f458c14
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2006-06-02 08:43:46 +0000

    Fix copyright.

--- istgt/kernel/iscsi_tcp_tgt.c
+++ istgt/kernel/iscsi_tcp_tgt.c
@@ -2,7 +2,7 @@
  * iSCSI Target over TCP/IP
  *
  * Copyright (C) 2006 FUJITA Tomonori <tomof@acm.org>
- * Copyright (C) 2006 Mike Christie
+ * Copyright (C) 2006 Mike Christie <michaelc@cs.wisc.edu>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
