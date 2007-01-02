27f989c38e56b8ecbc6cec6520aebbb1ba4b6aa5
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2007-01-02 08:26:40 +0000

    Add the makeshift readv/writev code.

--- usr/bd_xen.c
+++ usr/bd_xen.c
@@ -27,17 +27,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <libaio.h>
-
 #include <linux/fs.h>
 #include <sys/epoll.h>
+#include <sys/uio.h>
 
 #include "list.h"
 #include "util.h"
 #include "tgtd.h"
 
+#define O_DIRECT 040000 /* who defines this?*/
+
 static int bd_xen_open(struct tgt_device *dev, char *path, int *fd, uint64_t *size)
 {
-	*fd = backed_file_open(path, O_RDWR| O_LARGEFILE, size);
+	*fd = backed_file_open(path, O_RDWR| O_LARGEFILE | O_DIRECT, size);
 
 	return *fd >= 0 ? 0 : *fd;
 }
@@ -47,10 +49,29 @@ static void bd_xen_close(struct tgt_device *dev)
 	close(dev->fd);
 }
 
+/*
+ * Replace this with AIO readv/writev after 2.6.20.
+ */
 static int bd_xen_cmd_submit(struct tgt_device *dev, uint8_t *scb, int rw,
 			     uint32_t datalen, unsigned long *uaddr,
 			     uint64_t offset, int *async, void *key)
 {
+	struct iovec *iov = (struct iovec *) (void *) *uaddr;
+	int cnt;
+	long total;
+
+	cnt = total = 0;
+	do {
+		total += iov[cnt++].iov_len;
+	} while (total < datalen);
+
+	lseek64(dev->fd, offset, SEEK_SET);
+
+	if (rw == READ)
+		readv(dev->fd, iov, cnt);
+	else
+		writev(dev->fd, iov, cnt);
+
 	return 0;
 }
 
