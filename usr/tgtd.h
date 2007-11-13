52b71d48ef33ea9371cd287fb907941beee90a2e
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2007-11-13 20:19:08 +0900

    add scsi_cmnd.h
    move scsi command stuff from tgtd.h to scsi_cmnd.h
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- usr/tgtd.h
+++ usr/tgtd.h
@@ -2,6 +2,7 @@
 #define __TARGET_DAEMON_H
 
 #include "log.h"
+#include "scsi_cmnd.h"
 
 #define SCSI_ID_LEN		24
 #define SCSI_SN_LEN		8
@@ -132,112 +133,6 @@ struct scsi_lu {
 	/* Currently only used by smc module */
 	void *smc_p;
 };
-
-enum data_direction {
-	DATA_NONE = 0,
-	DATA_WRITE = 1,
-	DATA_READ = 2,
-	DATA_BIDIRECTIONAL = 3,
-};
-
-struct scsi_cmd {
-	struct target *c_target;
-	/* linked it_nexus->cmd_hash_list */
-	struct list_head c_hlist;
-	struct list_head qlist;
-
-	uint64_t dev_id;
-
-	uint64_t uaddr;
-	uint32_t len;
-	int mmapped;
-	struct scsi_lu *dev;
-	unsigned long state;
-
-	uint32_t write_len;
-	uint32_t read_len;
-	enum data_direction data_dir;
-
-	uint64_t cmd_itn_id;
-	uint64_t offset;
-	uint8_t *scb;
-	int scb_len;
-	uint8_t lun[8];
-	int attribute;
-	uint64_t tag;
-	int async;
-	int result;
-	struct mgmt_req *mreq;
-
-#define SCSI_SENSE_BUFFERSIZE	252
-	unsigned char sense_buffer[SCSI_SENSE_BUFFERSIZE];
-	int sense_len;
-
-	/* workaround */
-	struct list_head bs_list;
-};
-
-static inline void scsi_set_data_dir(struct scsi_cmd *scmd,
-				     enum data_direction dir)
-{
-	scmd->data_dir = dir;
-}
-
-static inline enum data_direction scsi_get_data_dir(struct scsi_cmd *scmd)
-{
-	return scmd->data_dir;
-}
-
-static inline void scsi_set_result(struct scsi_cmd *scmd,
-				   int result)
-{
-	scmd->result = result;
-}
-
-static inline int scsi_get_result(struct scsi_cmd *scmd)
-{
-	return scmd->result;
-}
-
-static inline void scsi_set_read_len(struct scsi_cmd *scmd, uint32_t read_len)
-{
-	scmd->read_len = read_len;
-}
-
-static inline uint32_t scsi_get_read_len(struct scsi_cmd *scmd)
-{
-	return scmd->read_len;
-}
-
-static inline void scsi_set_write_len(struct scsi_cmd *scmd, uint32_t write_len)
-{
-	scmd->write_len = write_len;
-}
-
-static inline uint32_t scsi_get_write_len(struct scsi_cmd *scmd)
-{
-	return scmd->write_len;
-}
-
-static inline void scsi_set_read_buffer(struct scsi_cmd *scmd, void *addr)
-{
-	scmd->uaddr = (unsigned long)addr;
-}
-
-static inline void *scsi_get_read_buffer(struct scsi_cmd *scmd)
-{
-	return (void *)(unsigned long)scmd->uaddr;
-}
-
-static inline void scsi_set_write_buffer(struct scsi_cmd *scmd, void *addr)
-{
-	scmd->uaddr = (unsigned long)addr;
-}
-
-static inline void *scsi_get_write_buffer(struct scsi_cmd *scmd)
-{
-	return (void *)(unsigned long)scmd->uaddr;
-}
 
 struct mgmt_req {
 	uint64_t mid;
