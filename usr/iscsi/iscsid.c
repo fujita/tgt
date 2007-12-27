fujita/tgt

commit 9c198d43959fb6991815a76f7eb6dcba0b98ff26

iscsi: move task allocation to transport
Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

usr/iscsi/iscsid.c
@@ -1030,15 +1030,14 @@ static struct iscsi_task *iscsi_alloc_task(struct iscsi_connection *conn,
 	struct iscsi_task *task;
 	void *buf;
 
-	task = malloc(sizeof(*task) + conn->tp->task_trans_len + ext_len);
+	task = conn->tp->alloc_task(conn, ext_len);
 	if (!task)
 		return NULL;
-	memset(task, 0, sizeof(*task) + conn->tp->task_trans_len + ext_len);
 
 	if (data_len) {
 		buf = conn->tp->alloc_data_buf(conn, data_len);
 		if (!buf) {
-			free(task);
+			conn->tp->free_task(task);
 			return NULL;
 		}
 		task->data = buf;
@@ -1048,13 +1047,6 @@ static struct iscsi_task *iscsi_alloc_task(struct iscsi_connection *conn,
 	task->conn = conn;
 	INIT_LIST_HEAD(&task->c_hlist);
 	INIT_LIST_HEAD(&task->c_list);
-
-	if (conn->tp->task_trans_len)
-		task->trans_data = (void *) &task[1];
-
-	if (ext_len)
-		task->extdata = (void *)((uintptr_t) &task[1] +
-				      conn->tp->task_trans_len);
 
 	conn_get(conn);
 	return task;
@@ -1067,7 +1059,7 @@ void iscsi_free_task(struct iscsi_task *task)
 	conn->tp->free_data_buf(conn, scsi_get_in_buffer(&task->scmd));
 	conn->tp->free_data_buf(conn, scsi_get_out_buffer(&task->scmd));
 
-	free(task);
+	conn->tp->free_task(task);
 	conn_put(conn);
 }
 
@@ -1482,8 +1474,6 @@ static int iscsi_scsi_cmd_rx_start(struct iscsi_connection *conn)
 		return -ENOMEM;
 
 	task->tag = req->itt;
-
-	conn->tp->ep_task_init(task);
 
 	if (ahs_len) {
 		task->ahs = task->extdata + sizeof(req->cdb);

usr/iscsi/iscsi_rdma.c
@@ -105,6 +105,7 @@ struct iser_task {
 	/* read and write from the initiator's point of view */
 	uint32_t rem_read_stag, rem_write_stag;
 	uint64_t rem_read_va, rem_write_va;
+	struct iscsi_task task;
 };
 
 struct iser_device;
@@ -256,6 +257,11 @@ static int num_rx_ready;
  */
 static int mempool_num = 192;
 static size_t mempool_size = 512 * 1024;
+
+static inline struct iser_task *ISER_TASK(struct iscsi_task *t)
+{
+	return container_of(t, struct iser_task, task);
+}
 
 static inline struct conn_info *RDMA_CONN(struct iscsi_connection *conn)
 {
@@ -1224,15 +1230,27 @@ out:
 /*
  * Copy the remote va and stag that were temporarily saved in conn_info.
  */
-static void iscsi_iser_task_init(struct iscsi_task *task)
+struct iscsi_task *iscsi_iser_alloc_task(struct iscsi_connection *conn,
+					 size_t ext_len)
 {
-	struct conn_info *ci = RDMA_CONN(task->conn);
-	struct iser_task *itask = task->trans_data;
+	struct conn_info *ci = RDMA_CONN(conn);
+	struct iser_task *itask;
+
+	itask = zalloc(sizeof(*itask) + ext_len);
+	if (!itask)
+		return NULL;
 
 	itask->rem_read_stag = ci->rem_read_stag;
 	itask->rem_read_va = ci->rem_read_va;
 	itask->rem_write_stag = ci->rem_write_stag;
 	itask->rem_write_va = ci->rem_write_va;
+
+	return &itask->task;
+}
+
+static void iscsi_iser_free_task(struct iscsi_task *task)
+{
+	free(ISER_TASK(task));
 }
 
 static int iser_parse_hdr(struct conn_info *ci, struct recvlist *recvl)
@@ -1423,7 +1441,7 @@ static int iscsi_rdma_rdma_read(struct iscsi_connection *conn)
 {
 	struct conn_info *ci = RDMA_CONN(conn);
 	struct iscsi_task *task = conn->tx_task;
-	struct iser_task *itask = task->trans_data;
+	struct iser_task *itask = ISER_TASK(task);
 	struct iscsi_r2t_rsp *r2t = (struct iscsi_r2t_rsp *) &conn->rsp.bhs;
 	uint8_t *buf;
 	uint32_t len;
@@ -1459,7 +1477,7 @@ static int iscsi_rdma_rdma_write(struct iscsi_connection *conn)
 {
 	struct conn_info *ci = RDMA_CONN(conn);
 	struct iscsi_task *task = conn->tx_task;
-	struct iser_task *itask = task->trans_data;
+	struct iser_task *itask = ISER_TASK(task);
 	struct iscsi_pdu *rsp = &conn->rsp;
 	struct iscsi_data_rsp *datain = (struct iscsi_data_rsp *) &rsp->bhs;
 	uint32_t offset;
@@ -1689,11 +1707,11 @@ static int iscsi_rdma_getpeername(struct iscsi_connection *conn,
 struct iscsi_transport iscsi_iser = {
 	.name			= "iser",
 	.rdma			= 1,
-	.task_trans_len		= sizeof(struct iser_task),
 	.data_padding		= 1,
 	.ep_init		= iscsi_rdma_init,
 	.ep_login_complete	= iscsi_rdma_login_complete,
-	.ep_task_init		= iscsi_iser_task_init,
+	.alloc_task		= iscsi_iser_alloc_task,
+	.free_task		= iscsi_iser_free_task,
 	.ep_read		= iscsi_iser_read,
 	.ep_write_begin		= iscsi_iser_write_begin,
 	.ep_write_end		= iscsi_iser_write_end,

usr/iscsi/iscsi_tcp.c
@@ -226,10 +226,6 @@ static int iscsi_tcp_conn_login_complete(struct iscsi_connection *conn)
 	return 0;
 }
 
-static void iscsi_tcp_task_init(struct iscsi_task *task)
-{
-}
-
 static size_t iscsi_tcp_read(struct iscsi_connection *conn, void *buf,
 			     size_t nbytes)
 {
@@ -308,6 +304,22 @@ void iscsi_event_modify(struct iscsi_connection *conn, int events)
 		eprintf("tgt_event_modify failed\n");
 }
 
+struct iscsi_task *iscsi_tcp_alloc_task(struct iscsi_connection *conn,
+					size_t ext_len)
+{
+	struct iscsi_task *task;
+
+	task = malloc(sizeof(*task) + ext_len);
+	if (task)
+		memset(task, 0, sizeof(*task) + ext_len);
+	return task;
+}
+
+static void iscsi_tcp_free_task(struct iscsi_task *task)
+{
+	free(task);
+}
+
 void *iscsi_tcp_alloc_data_buf(struct iscsi_connection *conn, size_t sz)
 {
 	return valloc(sz);
@@ -338,11 +350,11 @@ int iscsi_tcp_getpeername(struct iscsi_connection *conn, struct sockaddr *sa,
 struct iscsi_transport iscsi_tcp = {
 	.name			= "iscsi",
 	.rdma			= 0,
-	.task_trans_len		= 0,
 	.data_padding		= PAD_WORD_LEN,
 	.ep_init		= iscsi_tcp_init,
 	.ep_login_complete	= iscsi_tcp_conn_login_complete,
-	.ep_task_init		= iscsi_tcp_task_init,
+	.alloc_task		= iscsi_tcp_alloc_task,
+	.free_task		= iscsi_tcp_free_task,
 	.ep_read		= iscsi_tcp_read,
 	.ep_write_begin		= iscsi_tcp_write_begin,
 	.ep_write_end		= iscsi_tcp_write_end,
@@ -350,6 +362,8 @@ struct iscsi_transport iscsi_tcp = {
 	.ep_release		= iscsi_tcp_release,
 	.ep_show		= iscsi_tcp_show,
 	.ep_event_modify	= iscsi_event_modify,
+	.alloc_task		= iscsi_tcp_alloc_task,
+	.free_task		= iscsi_tcp_free_task,
 	.alloc_data_buf		= iscsi_tcp_alloc_data_buf,
 	.free_data_buf		= iscsi_tcp_free_data_buf,
 	.ep_getsockname		= iscsi_tcp_getsockname,

usr/iscsi/iscsid.h
@@ -120,12 +120,12 @@ struct iscsi_task {
 	int unsol_count;
 	int exp_r2tsn;
 
-	struct scsi_cmd scmd;
-
-	void *trans_data;   /* transport specific data */
-	void *extdata;      /* extra data, after transport, for ahs only */
 	void *ahs;
 	void *data;
+
+	struct scsi_cmd scmd;
+
+	unsigned long extdata[0];
 };
 
 struct iscsi_connection {

usr/iscsi/transport.h
@@ -9,12 +9,13 @@ struct iscsi_task;
 struct iscsi_transport {
 	const char *name;
 	int rdma;
-	int task_trans_len;
 	int data_padding;
 
 	int (*ep_init) (void);
 	int (*ep_login_complete)(struct iscsi_connection *conn);
-	void (*ep_task_init)(struct iscsi_task *task);
+	struct iscsi_task *(*alloc_task)(struct iscsi_connection *conn,
+					 size_t ext_len);
+	void (*free_task)(struct iscsi_task *task);
 	size_t (*ep_read)(struct iscsi_connection *conn, void *buf,
 			  size_t nbytes);
 	size_t (*ep_write_begin)(struct iscsi_connection *conn, void *buf,

commit eee92b08e187481e1cb095c1432d3e6f338780fb

iscsi: fix ahs offset
Commit 9c198d43959fb6991815a76f7eb6dcba0b98ff26 introduced a bug by
changing extdata from a void * to a long *.  The pointer addition to
calculate the offset of the AHS in extended CDBs was off by a factor
of 8, leading to memory scribbles.  Fix with an explicit cast.

Signed-off-by: Pete Wyckoff <pw@osc.edu>
Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

usr/iscsi/iscsid.c
@@ -1476,7 +1476,7 @@ static int iscsi_scsi_cmd_rx_start(struct iscsi_connection *conn)
 	task->tag = req->itt;
 
 	if (ahs_len) {
-		task->ahs = task->extdata + sizeof(req->cdb);
+		task->ahs = (uint8_t *) task->extdata + sizeof(req->cdb);
 		conn->req.ahs = task->ahs;
 		conn->req.data = task->data;
 	} else if (data_len)