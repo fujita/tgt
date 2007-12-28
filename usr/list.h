d15101c72d5c389278b51c5b53a71788b8c8f43f
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2007-12-29 01:54:30 +0900

    add list_first_entry macro
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- usr/list.h
+++ usr/list.h
@@ -29,6 +29,9 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 	list->prev = list;
 }
 
+#define list_first_entry(ptr, type, member) \
+	list_entry((ptr)->next, type, member)
+
 static inline int list_empty(const struct list_head *head)
 {
 	return head->next == head;
