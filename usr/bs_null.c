092211a13b32c0fbd7b4680443c7b6d69edb6c22
Alexander Nezhinsky <nezhinsky@gmail.com>
2008-10-04 19:52:50 +0900

    add null backing store
    Implements a NULL I/O backing store, which shortcuts all scsi commands
    and returns a success status without actually performing them.
    Useful for performance measurements, by eliminating or significantly
    reducing I/O side bottleneck.
    
    Not based on bs_threads, commands are handled immediately from
    the context they are submitted from.
    
    Comparison with the thread-based implementation shows the impact of
    threading associated overhead on performance:
    Read,  thread: 69kIOPS, non-thread:72kIOPS
    Write, thread: 47kIOPS, non-thread:59kIOPS
    In both Read and Write tests, CPU util. dropped from ~30% to ~10%
    when switched to the non-thread based model.
    
    To add a device backed by this, "--bstype null" should be used with
    tgtadm, also a dummy device name (any string) should be supplied,
    because bs_null does not use any real devices but the current code
    won't admit a null-string device name.
    
    Signed-off-by: Alexander Nezhinsky <nezhinsky@gmail.com>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

---
+++ usr/bs_null.c
@@ -0,0 +1,83 @@
+/*
+ * NULL I/O backing store routine
+ *
+ * Copyright (C) 2008 Alexander Nezhinsky <nezhinskyf@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#include <errno.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+#include "tgtd.h"
+#include "scsi.h"
+
+#define NULL_BS_DEV_SIZE        (1ULL << 40)
+
+int bs_null_cmd_submit(struct scsi_cmd *cmd)
+{
+	int result = SAM_STAT_GOOD;
+	uint16_t asc = 0;
+	uint8_t key = 0;
+	uint8_t scb_op = cmd->scb[0];
+
+	set_cmd_async(cmd);
+
+	scsi_set_result(cmd, result);
+
+	if (result != SAM_STAT_GOOD) {
+		eprintf("io error %p %x %" PRIu64 ", %m\n",
+			cmd, scb_op, cmd->offset);
+		sense_data_build(cmd, key, asc);
+	}
+
+	cmd->scsi_cmd_done(cmd, scsi_get_result(cmd));
+	return 0;
+}
+
+static int bs_null_open(struct scsi_lu *lu, char *path,
+			int *fd, uint64_t *size)
+{
+	*size = NULL_BS_DEV_SIZE;
+	dprintf("NULL backing store open, size: %" PRIu64 "\n", *size);
+	return 0;
+}
+
+static void bs_null_close(struct scsi_lu *lu)
+{
+}
+
+static int bs_null_cmd_done(struct scsi_cmd *cmd)
+{
+	return 0;
+}
+
+static struct backingstore_template null_bst = {
+	.bs_name		= "null",
+	.bs_datasize		= 0,
+	.bs_open		= bs_null_open,
+	.bs_close		= bs_null_close,
+	.bs_cmd_submit		= bs_null_cmd_submit,
+	.bs_cmd_done		= bs_null_cmd_done,
+};
+
+__attribute__((constructor)) static void bs_null_constructor(void)
+{
+	register_backingstore_template(&null_bst);
+}
