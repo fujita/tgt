fujita/tgt

commit 415fd0033538ecc49329b1dce8d556c850c1d6c8

remove dirty __NR_sync_file_range hack
Now the majority of distributions has glibc that supports
sync_file_range() so let's make things just work on any architectures.

Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

usr/util.h
@@ -2,6 +2,7 @@
 #define __UTIL_H__
 
 #include <byteswap.h>
+#include <fcntl.h>
 #include <syscall.h>
 #include <unistd.h>
 #include <errno.h>
@@ -93,42 +94,20 @@ do {									\
 
 extern unsigned long pagesize, pageshift;
 
-
-/*
- * the latest glibc have a proper sync_file_range definition but
- * most of the distributions aren't shipped with it yet.
-*/
-
-#ifndef __NR_sync_file_range
-#if defined(__i386)
-#define __NR_sync_file_range	314
-#elif defined(__x86_64__)
-#define __NR_sync_file_range	277
-#elif defined(__ia64__)
-#define __NR_sync_file_range	1300
-#elif defined(__powerpc64__) || defined(__PPC__)
-#define __NR_sync_file_range	308
-#endif
-#endif
-
-#ifndef SYNC_FILE_RANGE_WAIT_BEFORE
-#define SYNC_FILE_RANGE_WAIT_BEFORE	1
-#define SYNC_FILE_RANGE_WRITE		2
-#define SYNC_FILE_RANGE_WAIT_AFTER	4
-#endif
-
-extern long int syscall(long int sysno, ...);
-
+#ifdef SYNC_FILE_RANGE_WAIT_BEFORE
 static inline int __sync_file_range(int fd, __off64_t offset, __off64_t bytes)
 {
 	int ret;
 	unsigned int flags = SYNC_FILE_RANGE_WAIT_BEFORE | SYNC_FILE_RANGE_WRITE
 		| SYNC_FILE_RANGE_WAIT_AFTER;
 
-	ret = syscall(__NR_sync_file_range, fd, offset, bytes, flags);
+	ret = sync_file_range(fd, offset, bytes, flags);
 	if (ret)
 		ret = fsync(fd);
 	return ret;
 }
+#else
+#define __sync_file_range(fd, offset, bytes) fsync(fd)
+#endif
 
 #endif

commit 455a170556a7f4eebacc11ab7e41f09aae5e82d5

fix sync_file_range regression
The commit 415fd0033538ecc49329b1dce8d556c850c1d6c8 introduced a
regression that breaks systems having libc that doesn't support
sync_file_range().

Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

usr/util.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <endian.h>
+#include <sys/syscall.h>
 #include "be_byteshift.h"
 
 #define roundup(x, y) ((((x) + ((y) - 1)) / (y)) * (y))
@@ -94,14 +95,18 @@ do {									\
 
 extern unsigned long pagesize, pageshift;
 
-#ifdef SYNC_FILE_RANGE_WAIT_BEFORE
+#if defined(__NR_sync_file_range) || defined(__NR_sync_file_range2)
 static inline int __sync_file_range(int fd, __off64_t offset, __off64_t bytes)
 {
 	int ret;
+#if defined(__NR_sync_file_range)
+	long int n = __NR_sync_file_range;
+#else
+	long int n = __NR_sync_file_range2;
+#endif
 	unsigned int flags = SYNC_FILE_RANGE_WAIT_BEFORE | SYNC_FILE_RANGE_WRITE
 		| SYNC_FILE_RANGE_WAIT_AFTER;
-
-	ret = sync_file_range(fd, offset, bytes, flags);
+	ret = syscall(n, fd, offset, bytes, flags);
 	if (ret)
 		ret = fsync(fd);
 	return ret;