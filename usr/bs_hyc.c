30509888899fa3fbaa1a6d5d186a34de2539afbc
Rohan Puri <rohan.puri15@gmail.com>
2018-02-09 14:57:06 +0530

    Added hyc backing store code.
    Now read/write scsi cmds are executed by calling respective storage lib APIs.
    
    Signed-off-by: Rohan Puri <rohan.puri@primaryio.com>

--- usr/bs_hyc.c
+++ usr/bs_hyc.c
@@ -27,6 +27,8 @@
 #include <sys/eventfd.h>
 #include <sys/epoll.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <pthread.h>
 
 #include "list.h"
 #include "tgtd.h"
@@ -36,25 +38,194 @@
 
 #include "bs_hyc.h"
 
+#include "TgtInterface.h"
+
 static inline struct bs_hyc_info *BS_HYC_I(struct scsi_lu *lu)
 {
 	return (struct bs_hyc_info *) ((char *)lu + sizeof (*lu));
 }
 
-static int bs_hyc_cmd_submit(struct scsi_cmd *cmd)
+io_type_t scsi_cmd_operation(struct scsi_cmd *cmdp)
 {
-	scsi_set_result(cmd, SAM_STAT_GOOD);
+	unsigned int        scsi_op = (unsigned int) cmdp->scb[0];
+	io_type_t           op = UNKNOWN;
+
+	switch (scsi_op) {
+	case WRITE_6:
+	case WRITE_10:
+	case WRITE_12:
+	case WRITE_16:
+		op = WRITE;
+		break;
+	case READ_6:
+	case READ_10:
+	case READ_12:
+	case READ_16:
+		op = READ;
+		break;
+	case WRITE_SAME:
+	case WRITE_SAME_16:
+		/** WRITE_SAME used to punch a hole in file */
+		if (cmdp->scb[1] & 0x08) {
+			eprintf("Unmap with WRITE_SAME for hyc backend is not"
+				" supported yet.\n");
+			op = UNKNOWN;
+			goto out;
+		}
+		op = WRITE_SAME_OP;
+		break;
+	case SYNCHRONIZE_CACHE:
+	case SYNCHRONIZE_CACHE_16:
+	default:
+		eprintf("skipped cmd: %p op: %x\n", cmdp, scsi_op);
+		op = UNKNOWN;
+	}
+out:
+	return op;
+}
+
+static uint64_t scsi_cmd_offset(struct scsi_cmd *cmdp)
+{
+	return cmdp->offset;
+}
+
+static uint32_t scsi_cmd_length(struct scsi_cmd *cmdp)
+{
+	switch (scsi_cmd_operation(cmdp)) {
+	case READ:
+		return scsi_get_in_length(cmdp);
+	case WRITE_SAME_OP:
+	case WRITE:
+		return scsi_get_out_transfer_len(cmdp);
+	case UNKNOWN:
+		assert(0);
+	}
+	return 0;
+}
+
+struct hyc_cmd *hyc_cmd_alloc(struct bs_hyc_info *infop, struct scsi_cmd *cmdp,
+	io_type_t op)
+{
+	struct hyc_cmd *hcmdp = NULL;
+
+	hcmdp = calloc(1, sizeof(*hcmdp));
+	if (!hcmdp) {
+		return NULL;
+	}
+
+	DLL_INIT(&hcmdp->list);
+
+	hcmdp->cmdp = cmdp;
+	hcmdp->infop = infop;
+	hcmdp->op = op;
+
+	return hcmdp;
+}
+
+static char *scsi_cmd_buffer(struct scsi_cmd *cmdp)
+{
+	switch (scsi_cmd_operation(cmdp)) {
+	default:
+		assert(0);
+		return NULL;
+	case READ:
+		return scsi_get_in_buffer(cmdp);
+	case WRITE:
+	case WRITE_SAME_OP:
+		return scsi_get_out_buffer(cmdp);
+	}
+}
+
+static int bs_hyc_cmd_submit(struct scsi_cmd *cmdp)
+{
+	struct scsi_lu     *lup = NULL;
+	struct bs_hyc_info *infop = NULL;
+	io_type_t           op;
+	size_t              length;
+	uint64_t            offset;
+	char               *bufp = NULL;
+	struct hyc_cmd     *hcmdp = NULL;
+	RequestID           reqid = kInvalidRequestID;
+
+	lup = cmdp->dev;
+	infop = BS_HYC_I(lup);
+
+	assert(infop->vmdk != kInvalidVmdkHandle);
+
+	op = scsi_cmd_operation(cmdp);
+	if (op == UNKNOWN) {
+		return -EINVAL;
+	}
+
+	hcmdp = hyc_cmd_alloc(infop, cmdp, op);
+	if (!hcmdp) {
+		return -ENOMEM;
+	}
+
+	offset = scsi_cmd_offset(cmdp);
+	length = scsi_cmd_length(cmdp);
+
+	bufp = scsi_cmd_buffer(cmdp);
+	set_cmd_async(cmdp);
+
+	switch (op) {
+	case READ:
+		reqid = ScheduleRead(infop->vmdk, hcmdp, bufp, length, offset);
+		break;
+	case WRITE:
+		reqid = ScheduleWrite(infop->vmdk, hcmdp, bufp, length, offset);
+		break;
+	case WRITE_SAME_OP:
+		/** TODO */
+	case UNKNOWN:
+	default:
+		assert(0);
+	}
+
+	/* If we got reqid, set it in hyc_cmd */
+	if (reqid != kInvalidRequestID) {
+		hcmdp->reqid = reqid;
+	}
+
+	pthread_mutex_lock(&infop->lock);
+	DLL_ADD(&infop->sched_cmd_list, &hcmdp->list);
+	pthread_mutex_unlock(&infop->lock);
+
 	return 0;
 }
 
 static void bs_hyc_handle_completion(int fd, int events, void *datap)
 {
-	//struct bs_hyc_info *infop;
-	//struct scsi_cmd    *cmdp = NULL;
+	struct bs_hyc_info   *infop;
+	dll_t                 list;
+	uint64_t              c;
+	struct RequestResult *resultsp = NULL;
+	bool                  has_more = true; /** True for the first cmd completion */
+	int                   rc;
+	int                   i;
 
-	//infop = datap;
+	assert(datap);
+	infop = datap;
 
-	/** TODO: For all the scsi_cmds, call target_cmd_io_done() */
+	resultsp = infop->request_resultsp;
+	DLL_INIT(&list);
+
+	rc = eventfd_read(fd, &c);
+	assert(rc == 0);
+
+	while (has_more == true) {
+		uint32_t nr_results = GetCompleteRequests(infop->vmdk, resultsp,
+						infop->nr_results, &has_more);
+		/* Process completed request commands */
+		for (i = 0; i < nr_results; ++i) {
+			struct hyc_cmd *hcmdp = (struct hyc_cmd *)resultsp[i].privatep;
+			assert(hcmdp);
+
+			assert(resultsp[i].result == 0);
+			target_cmd_io_done(hcmdp->cmdp, SAM_STAT_GOOD);
+			free(hcmdp);
+		}
+	}
 }
 
 static int bs_hyc_open(struct scsi_lu *lup, char *pathp,
@@ -91,6 +262,8 @@ static int bs_hyc_open(struct scsi_lu *lup, char *pathp,
 		goto error;
 	}
 
+	infop->done_eventfd = efd;
+
 error:
 	if (efd >= 0) {
 		close(efd);
@@ -103,15 +276,25 @@ error:
 	return rc;
 }
 
-static void bs_hyc_close(struct scsi_lu *lu)
+static void bs_hyc_close(struct scsi_lu *lup)
 {
+	struct bs_hyc_info *infop = BS_HYC_I(lup);
 
+	assert(infop);
+	assert(infop->done_eventfd >= 0);
+
+	tgt_event_del(infop->done_eventfd);
+	close(infop->done_eventfd);
+	infop->done_eventfd = -1;
+
+	close(lup->fd);
 }
 
 static tgtadm_err bs_hyc_init(struct scsi_lu *lup, char *bsoptsp)
 {
 	struct bs_hyc_info *infop = BS_HYC_I(lup);
-	tgtadm_err          e;
+	tgtadm_err          e = TGTADM_SUCCESS;
+	int                 rc;
 
 	memset(infop, 0, sizeof(*infop));
 
@@ -119,17 +302,44 @@ static tgtadm_err bs_hyc_init(struct scsi_lu *lup, char *bsoptsp)
 
 	assert(lup->tgt);
 	assert(lup->tgt->vmid);
+	assert(lup->vmdkid);
 
 	infop->vmid = lup->tgt->vmid;
 	infop->vmdkid = lup->vmdkid;
+	infop->vmdk = GetVmdkHandle(infop->vmdkid);
+	assert(infop->vmdk != kInvalidVmdkHandle);
 
-	e = TGTADM_SUCCESS;
+	rc = pthread_mutex_init(&infop->lock, NULL);
+	assert(rc == 0);
+
+	DLL_INIT(&infop->sched_cmd_list);
+
+	/** TODO: Use a symbolic const over here */
+	infop->nr_results = 32;
+	infop->request_resultsp = calloc(infop->nr_results,
+					sizeof(*infop->request_resultsp));
+	if (!infop->request_resultsp) {
+		eprintf("hyc bs init failed\n");
+		e = TGTADM_NOMEM;
+	}
+
 	return e;
 }
 
 static void bs_hyc_exit(struct scsi_lu *lup)
 {
+	struct bs_hyc_info *infop = BS_HYC_I(lup);
+	int    rc;
 
+
+	assert(infop);
+	assert(DLL_ISEMPTY(&infop->sched_cmd_list));
+
+	free(infop->request_resultsp);
+
+	rc = pthread_mutex_destroy(&infop->lock);
+
+	assert(rc == 0);
 }
 
 static struct backingstore_template hyc_bst = {
